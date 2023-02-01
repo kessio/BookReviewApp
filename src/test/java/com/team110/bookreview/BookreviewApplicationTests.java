ab885b0b15ecfc4dfe36e66bbf7d4bec4df75779
thansen <thansendhrit.lahare161@gmail.com>
2023-02-02 00:07:41 +0530

    connected the application with docker compose

--- src/test/java/com/team110/bookreview/BookreviewApplicationTests.java
+++ src/test/java/com/team110/bookreview/BookreviewApplicationTests.java
@@ -1,12 +1,11 @@
 package com.team110.bookreview;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookreviewApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
