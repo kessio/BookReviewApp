064c647f70db7e159387099e5a684a89708b4def
unknown <sharonkessio6@gmail.com>
2023-02-01 14:39:59 +0300

    create register and welcome page, route it

---
+++ frontend/src/pages/WelcomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+function welcomePage() {
+  return (
+    <div>
+     <NavBar></NavBar>
+      <div>welcomePage</div>
+    </div>
+    
+  )
+}
+
+export default welcomePage
